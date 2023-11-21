2d7d2c2cc43921b42f79fa43a1f47a5cd84a3dd7
tolu-c <tolu.adeyemo7@gmail.com>
2023-11-21 15:03:24 +0100

    layout structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,27 @@
+import { Loader } from "components/ui/Loader";
+import { Fragment, Suspense, lazy, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+
+// routes
+const Home = lazy(() => import("./routes/Home"));
+const Login = lazy(() => import("./routes/Login"));
+const Register = lazy(() => import("./routes/Register"));
+const LoggedIn = lazy(() => import("./routes/LoggedIn"));
+
 function App() {
-  return <h1 className="text-xl font-bold font-lato">HealthGPT</h1>;
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(true);
+
+  return (
+    <Fragment>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="/*" element={isLoggedIn ? <LoggedIn /> : <Home />} />
+        </Routes>
+      </Suspense>
+    </Fragment>
+  );
 }
 
 export default App;
